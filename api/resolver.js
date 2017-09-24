be38862247109da7e419f308fe143efbc6615c90
jonathantsang <j26tsang@gmail.com>
2017-09-24 01:51:01 -0400

    load

--- api/resolver.js
+++ api/resolver.js
@@ -5,13 +5,12 @@ var _ = require('underscore');
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
-  term = JSON.parse(req);
-  console.log("term: " + term);
+  var term = req.query.text.trim();
+  console.log("term " + term);
   handleSearchString(term, req, res);
 };
 
 function handleSearchString(term, req, res) {
-  var table = '<table style="width:100%"><tr><th>Stock Name</th><th>'+ term.descrip +'</th></tr><tr><td>Asset Class</td><td>' + term.aClass + '</td></tr><tr><td>Asset Type</td><td>' + term.aType +'</td></tr><tr><td>Sector</td><td>' + term.sect + '</td></td><tr><td>Currency</td><td>' + term.curr + '</td></td></table>';
   console.log(table);
   res.json({
     body: table
--- api/typeahead.js
+++ api/typeahead.js
@@ -45,7 +45,9 @@ module.exports = function(req, res) {
       };
       var table = '<table style="width:100%"><tr><th>Stock Name</th><th>'+ term.descrip +'</th></tr><tr><td>Asset Class</td><td>' + term.aClass + '</td></tr><tr><td>Asset Type</td><td>' + term.aType +'</td></tr><tr><td>Sector</td><td>' + term.sect + '</td></td><tr><td>Currency</td><td>' + term.curr + '</td></td></table>';
       console.log(table);
-      res.json(data);
+      res.json({
+        title: table
+      });
     } else {
       console.log("incomplete");
       res.json([{
