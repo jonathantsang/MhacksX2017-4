3a680dd9a761d38f65bf743913956b4384d79359
jonathantsang <j26tsang@gmail.com>
2017-09-24 01:47:11 -0400

    load

--- api/resolver.js
+++ api/resolver.js
@@ -6,7 +6,7 @@ var _ = require('underscore');
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
   term = req;
-  console.log(term);
+  console.log("term: " + term);
   handleSearchString(term, req, res);
 };
 
